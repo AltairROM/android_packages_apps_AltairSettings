d03ca382f56f1b6bd465d26449dc4f6a4a5d79af
Martin J. Pollard <martin0566@gmail.com>
2018-08-02 18:47:21 -0400

    Moved status bar battery and clock settings to Status Bar tab

--- src/com/altair/settings/tabs/StatusBar.java
+++ src/com/altair/settings/tabs/StatusBar.java
@@ -16,25 +16,53 @@
 
 package com.altair.settings.tabs;
 
+//import android.app.ActivityManager;
 import android.content.Context;
 import android.content.ContentResolver;
-import android.content.res.Resources;
+//import android.content.res.Resources;
 import android.os.Bundle;
 import android.os.UserHandle;
 import android.provider.Settings;
 import android.support.v7.preference.Preference;
+import android.support.v14.preference.SwitchPreference;
+import android.text.format.DateFormat;
+//import android.text.TextUtils;
+//import android.util.ArraySet;
+//import android.util.AttributeSet;
+import android.view.View;
 
+//import java.util.Set;
+
+import com.android.internal.logging.nano.MetricsProto;
 import com.android.settings.R;
 import com.android.settings.SettingsPreferenceFragment;
-import com.android.internal.logging.nano.MetricsProto;
-import com.android.settings.Utils;
+//import com.android.settings.Utils;
 
 import com.altair.settings.preference.CustomSeekBarPreference;
+import com.altair.settings.StatusBarIcon;
+
+import lineageos.preference.LineageSystemSettingListPreference;
+import lineageos.preference.SecureSettingSwitchPreference;
 
 public class StatusBar extends SettingsPreferenceFragment implements
         Preference.OnPreferenceChangeListener {
     private static final String TAG = "StatusBar";
 
+    private static final String STATUS_BAR_SHOW_CLOCK = "status_bar_show_clock";
+    private static final String STATUS_BAR_CLOCK = "status_bar_clock";
+    private static final String CLOCK_SECONDS = "clock_seconds";
+    private static final String STATUS_BAR_AM_PM = "status_bar_am_pm";
+    private static final String STATUS_BAR_SHOW_BATTERY = "status_bar_show_battery";
+
+    private StatusBarIcon mClockIcon;
+    private StatusBarIcon mBatteryIcon;
+    private boolean m24HourClock;
+
+    private LineageSystemSettingListPreference mStatusBarClock;
+    private LineageSystemSettingListPreference mStatusBarAmPm;
+    private SwitchPreference mStatusBarShowClock;
+    private SwitchPreference mStatusBarShowBattery;
+    private SwitchPreference mBatteryPercentage;
     private CustomSeekBarPreference mQsRowsPort;
     private CustomSeekBarPreference mQsRowsLand;
     private CustomSeekBarPreference mQsColumnsPort;
@@ -45,6 +73,31 @@ public class StatusBar extends SettingsPreferenceFragment implements
         super.onCreate(savedInstanceState);
         addPreferencesFromResource(R.xml.tab_status_bar);
         ContentResolver resolver = getActivity().getContentResolver();
+
+        mClockIcon = new StatusBarIcon(getContext(), "clock");
+
+        mStatusBarShowClock =
+                (SwitchPreference) findPreference(STATUS_BAR_SHOW_CLOCK);
+        mStatusBarShowClock.setOnPreferenceChangeListener(this);
+        
+        mStatusBarAmPm =
+                (LineageSystemSettingListPreference) findPreference(STATUS_BAR_AM_PM);
+        mStatusBarClock =
+                (LineageSystemSettingListPreference) findPreference(STATUS_BAR_CLOCK);
+
+        m24HourClock = DateFormat.is24HourFormat(getActivity());
+
+        mBatteryIcon = new StatusBarIcon(getContext(), "battery");
+
+        mStatusBarShowBattery =
+                (SwitchPreference) findPreference(STATUS_BAR_SHOW_BATTERY);
+        mStatusBarShowBattery.setOnPreferenceChangeListener(this);
+
+        boolean show = Settings.System.getInt(resolver,
+                Settings.System.SHOW_BATTERY_PERCENT, 1) == 1;
+        mBatteryPercentage = (SwitchPreference) findPreference("show_battery_percent");
+        mBatteryPercentage.setChecked(show);
+        mBatteryPercentage.setOnPreferenceChangeListener(this);
 
         int value = Settings.System.getIntForUser(resolver,
                 Settings.System.QS_ROWS_PORTRAIT, 3, UserHandle.USER_CURRENT);
@@ -79,6 +132,34 @@ public class StatusBar extends SettingsPreferenceFragment implements
     @Override
     public void onResume() {
         super.onResume();
+
+        boolean iconEnabled = mClockIcon.isEnabled();
+        mStatusBarShowClock.setChecked(iconEnabled);
+
+        if (m24HourClock) {
+            mStatusBarAmPm.setEnabled(false);
+            mStatusBarAmPm.setSummary(R.string.status_bar_am_pm_info);
+        }
+
+        final boolean hasNotch = getResources().getBoolean(
+                org.lineageos.platform.internal.R.bool.config_haveNotch);
+
+        // Adjust status bar preferences for RTL
+        if (getResources().getConfiguration().getLayoutDirection() == View.LAYOUT_DIRECTION_RTL) {
+            if (hasNotch) {
+                mStatusBarClock.setEntries(R.array.status_bar_clock_position_entries_notch_rtl);
+                mStatusBarClock.setEntryValues(R.array.status_bar_clock_position_values_notch_rtl);
+            } else {
+                mStatusBarClock.setEntries(R.array.status_bar_clock_position_entries_rtl);
+                mStatusBarClock.setEntryValues(R.array.status_bar_clock_position_values_rtl);
+            }
+        } else if (hasNotch) {
+            mStatusBarClock.setEntries(R.array.status_bar_clock_position_entries_notch);
+            mStatusBarClock.setEntryValues(R.array.status_bar_clock_position_values_notch);
+        }
+
+        iconEnabled = mBatteryIcon.isEnabled();
+        mStatusBarShowBattery.setChecked(iconEnabled);
     }
 
     @Override
@@ -88,7 +169,21 @@ public class StatusBar extends SettingsPreferenceFragment implements
 
     public boolean onPreferenceChange(Preference preference, Object newValue) {
         final ContentResolver resolver = getActivity().getContentResolver();
-        if (preference == mQsRowsPort) {
+        if (preference == mStatusBarShowClock) {
+            mClockIcon.setEnabled((Boolean) newValue);
+            if (m24HourClock) {
+                mStatusBarAmPm.setEnabled(false);
+            }
+            return true;
+        } else if (preference == mStatusBarShowBattery) {
+            mBatteryIcon.setEnabled((Boolean) newValue);
+            return true;
+        } else if (preference == mBatteryPercentage) {
+            boolean value = (Boolean) newValue;
+            Settings.System.putInt(getActivity().getContentResolver(),
+                    Settings.System.SHOW_BATTERY_PERCENT, value ? 1 : 0);
+            return true;
+        } else if (preference == mQsRowsPort) {
             int val = (Integer) newValue;
             Settings.System.putIntForUser(getContentResolver(),
                     Settings.System.QS_ROWS_PORTRAIT, val, UserHandle.USER_CURRENT);
