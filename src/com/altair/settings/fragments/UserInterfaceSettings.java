7f622fe0c4bb0099037e1ee986b16aec42f5b9ef
Martin J. Pollard <martin0566@gmail.com>
2020-04-07 16:45:54 -0400

    Added recents style configuration to User Interface page
    Select between Pie and Oreo-style recent apps style. Oreo style also
    allows displaying a free memory bar, a "clear all" button, and displaying
    recents in stock, grid, or Android Go layouts.
    
    Adapted to AltairSettings from Resurrection Remix commits:
    Resurrection_packages_apps_Settings/commit/a339f90d80389ae6700290170aec46174d477c0e
    Resurrection_packages_apps_Settings/commit/849ac8048af2b5bd23c41cdadac9b31014716e03
    Resurrection_packages_apps_Settings/commit/a503ac15b32c78fb95acf29fcca346cabd294b2d
    Resurrection_packages_apps_Settings/commit/fac25aafffee39ace7101be91c18e31871fadf16
    Resurrection_packages_apps_Settings/commit/e5a745193de2c42113f0dc6cb55dfd2c5cc91a5a

--- src/com/altair/settings/fragments/UserInterfaceSettings.java
+++ src/com/altair/settings/fragments/UserInterfaceSettings.java
@@ -18,8 +18,13 @@ package com.altair.settings.fragments;
 
 import android.content.ContentResolver;
 import android.os.Bundle;
-import android.preference.Preference;
-import android.preference.Preference.OnPreferenceChangeListener;
+import android.os.UserHandle;
+import android.support.v7.preference.ListPreference;
+import android.support.v7.preference.Preference;
+import android.support.v7.preference.PreferenceCategory;
+import android.support.v7.preference.Preference.OnPreferenceChangeListener;
+import android.support.v7.preference.PreferenceScreen;
+import android.support.v14.preference.SwitchPreference;
 import android.provider.Settings;
 
 import com.android.internal.logging.nano.MetricsProto;
@@ -27,15 +32,58 @@ import com.android.internal.logging.nano.MetricsProto;
 import com.android.settings.R;
 import com.android.settings.SettingsPreferenceFragment;
 
+import com.altair.settings.utils.SystemUtils;
+
 public class UserInterfaceSettings extends SettingsPreferenceFragment implements
         Preference.OnPreferenceChangeListener {
     private static final String TAG = "UserInterface";
+
+    private static final String RECENTS_COMPONENT_TYPE = "recents_component";
+    private static final String RECENTS_TYPE = "recents_layout_style";
+    private static final String RECENTS_CLEAR_ALL_LOCATION = "recents_clear_all_location";
+
+    private static final int RECENTS_COMPONENT_OREO = 1;
+
+    private ListPreference mRecentsComponentType;
+    private ListPreference mRecentsClearAllLocation;
+    private SwitchPreference mRecentsClearAll;
+    private ListPreference mRecentsType;
+    private PreferenceCategory mOreoRecentsCat;
 
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         addPreferencesFromResource(R.xml.userinterface_settings);
-        ContentResolver resolver = getActivity().getContentResolver();
+
+        final ContentResolver resolver = getActivity().getContentResolver();
+        final PreferenceScreen prefSet = getPreferenceScreen();
+
+        // recents component type
+        mRecentsComponentType = (ListPreference) findPreference(RECENTS_COMPONENT_TYPE);
+        int type = Settings.System.getInt(resolver,
+                Settings.System.RECENTS_COMPONENT, 0);
+        mRecentsComponentType.setValue(String.valueOf(type));
+        mRecentsComponentType.setSummary(mRecentsComponentType.getEntry());
+        mRecentsComponentType.setOnPreferenceChangeListener(this);
+
+        // clear all recents
+        mRecentsClearAllLocation = (ListPreference) findPreference(RECENTS_CLEAR_ALL_LOCATION);
+        int location = Settings.System.getIntForUser(resolver,
+                Settings.System.RECENTS_CLEAR_ALL_LOCATION, 3, UserHandle.USER_CURRENT);
+        mRecentsClearAllLocation.setValue(String.valueOf(location));
+        mRecentsClearAllLocation.setSummary(mRecentsClearAllLocation.getEntry());
+        mRecentsClearAllLocation.setOnPreferenceChangeListener(this);
+
+        // oreo recents type
+        mRecentsType = (ListPreference) findPreference(RECENTS_TYPE);
+        int style = Settings.System.getIntForUser(resolver,
+                Settings.System.RECENTS_LAYOUT_STYLE, 0, UserHandle.USER_CURRENT);
+        mRecentsType.setValue(String.valueOf(style));
+        mRecentsType.setSummary(mRecentsType.getEntry());
+        mRecentsType.setOnPreferenceChangeListener(this);
+
+        mOreoRecentsCat = (PreferenceCategory)findPreference("recents_ui_oreo_recents_category");
+        mOreoRecentsCat.setEnabled(type == RECENTS_COMPONENT_OREO);
     }
 
     @Override
@@ -55,6 +103,35 @@ public class UserInterfaceSettings extends SettingsPreferenceFragment implements
 
     public boolean onPreferenceChange(Preference preference, Object objValue) {
         final String key = preference.getKey();
+        if (preference == mRecentsComponentType) {
+            int type = Integer.valueOf((String) objValue);
+            int index = mRecentsComponentType.findIndexOfValue((String) objValue);
+            Settings.System.putInt(getActivity().getContentResolver(),
+                    Settings.System.RECENTS_COMPONENT, type);
+            mRecentsComponentType.setSummary(mRecentsComponentType.getEntries()[index]);
+            mOreoRecentsCat.setEnabled(type == RECENTS_COMPONENT_OREO);
+            if (type == RECENTS_COMPONENT_OREO) { // Disable swipe up gesture, if oreo type selected
+                Settings.Secure.putInt(getActivity().getContentResolver(),
+                    Settings.Secure.SWIPE_UP_TO_SWITCH_APPS_ENABLED, 0);
+            }
+            SystemUtils.showSystemUiRestartDialog(getContext());
+            return true;
+        } else if (preference == mRecentsClearAllLocation) {
+            int location = Integer.valueOf((String) objValue);
+            int index = mRecentsClearAllLocation.findIndexOfValue((String) objValue);
+            Settings.System.putIntForUser(getActivity().getContentResolver(),
+                    Settings.System.RECENTS_CLEAR_ALL_LOCATION, location, UserHandle.USER_CURRENT);
+            mRecentsClearAllLocation.setSummary(mRecentsClearAllLocation.getEntries()[index]);
+            return true;
+        } else if (preference == mRecentsType) {
+            int style = Integer.valueOf((String) objValue);
+            int index = mRecentsType.findIndexOfValue((String) objValue);
+            Settings.System.putIntForUser(getActivity().getContentResolver(),
+                    Settings.System.RECENTS_LAYOUT_STYLE, style, UserHandle.USER_CURRENT);
+            mRecentsType.setSummary(mRecentsType.getEntries()[index]);
+            SystemUtils.showSystemUiRestartDialog(getContext());
+            return true;
+        }
         return true;
     }
 }
