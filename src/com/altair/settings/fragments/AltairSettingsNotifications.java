1aef04af878a02d4641ec061a65d3131a78106db
Martin J. Pollard <martin0566@gmail.com>
2026-02-19 15:03:35 -0500

    Notifications: Add flashlight blink on incoming calls

--- src/com/altair/settings/fragments/AltairSettingsNotifications.java
+++ src/com/altair/settings/fragments/AltairSettingsNotifications.java
@@ -1,22 +1,33 @@
 /*
- * SPDX-FileCopyrightText: 2019-2025 Altair ROM Project
+ * SPDX-FileCopyrightText: 2019-2026 Altair ROM Project
  * SPDX-License-Identifier: Apache-2.0
  */
 
 package com.altair.settings.fragments;
 
+import android.content.ContentResolver;
 import android.content.Context;
 import android.os.Bundle;
 import android.provider.SearchIndexableResource;
 import android.provider.Settings;
 
+import androidx.preference.ListPreference;
 import androidx.preference.Preference;
+import androidx.preference.PreferenceCategory;
+import androidx.preference.PreferenceScreen;
+import androidx.preference.Preference.OnPreferenceChangeListener;
+import androidx.preference.SwitchPreference;
 
 import com.android.internal.logging.nano.MetricsProto;
+import com.android.internal.util.custom.Utils;
 import com.android.settings.R;
 import com.android.settings.dashboard.DashboardFragment;
 import com.android.settings.search.BaseSearchIndexProvider;
 import com.android.settingslib.search.SearchIndexable;
+
+import com.lineage.support.preferences.SystemSettingListPreference;
+import com.lineage.support.preferences.SystemSettingSeekBarPreference;
+import com.lineage.support.preferences.SystemSettingSwitchPreference;
 
 import java.util.Arrays;
 import java.util.List;
@@ -26,6 +37,15 @@ public class AltairSettingsNotifications extends DashboardFragment implements
         Preference.OnPreferenceChangeListener {
     private static final String TAG = "AltairSettingsNotifications";
 
+    private static final String FLASHLIGHT_CATEGORY = "flashlight_category";
+    private static final String FLASHLIGHT_CALL_PREF = "flashlight_on_call";
+    private static final String FLASHLIGHT_DND_PREF = "flashlight_on_call_ignore_dnd";
+    private static final String FLASHLIGHT_RATE_PREF = "flashlight_on_call_rate";
+
+    private SystemSettingListPreference mFlashOnCall;
+    private SystemSettingSwitchPreference mFlashOnCallIgnoreDND;
+    private SystemSettingSeekBarPreference mFlashOnCallRate;
+
     @Override
     protected int getPreferenceScreenResId() {
         return R.xml.altair_settings_notifications;
@@ -34,6 +54,26 @@ public class AltairSettingsNotifications extends DashboardFragment implements
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+
+        final PreferenceScreen prefScreen = getPreferenceScreen();
+        final Context context = getActivity().getApplicationContext();
+        final ContentResolver resolver = context.getContentResolver();
+
+        if (!Utils.deviceHasFlashlight(context)) {
+            final PreferenceCategory flashlightCategory = prefScreen.findPreference(FLASHLIGHT_CATEGORY);
+            prefScreen.removePreference(flashlightCategory);
+        } else {
+            mFlashOnCall = (SystemSettingListPreference) prefScreen.findPreference(FLASHLIGHT_CALL_PREF);
+            mFlashOnCall.setOnPreferenceChangeListener(this);
+
+            mFlashOnCallIgnoreDND = (SystemSettingSwitchPreference) prefScreen.findPreference(FLASHLIGHT_DND_PREF);
+            int value = Settings.System.getInt(resolver, Settings.System.FLASHLIGHT_ON_CALL, 0);
+
+            mFlashOnCallRate = (SystemSettingSeekBarPreference) prefScreen.findPreference(FLASHLIGHT_RATE_PREF);
+
+            mFlashOnCallIgnoreDND.setEnabled(value > 1);
+            mFlashOnCallRate.setEnabled(value > 0);
+        }
     }
 
     @Override
@@ -63,6 +103,12 @@ public class AltairSettingsNotifications extends DashboardFragment implements
 
     @Override
     public boolean onPreferenceChange(Preference preference, Object newValue) {
+        if (preference == mFlashOnCall) {
+            int value = Integer.parseInt((String) newValue);
+            mFlashOnCallIgnoreDND.setEnabled(value > 1);
+            mFlashOnCallRate.setEnabled(value > 0);
+            return true;
+        }
         return false;
     }
 
@@ -84,6 +130,12 @@ public class AltairSettingsNotifications extends DashboardFragment implements
                 @Override
                 public List<String> getNonIndexableKeys(Context context) {
                     List<String> keys = super.getNonIndexableKeys(context);
+
+                    if (!Utils.deviceHasFlashlight(context)) {
+                        keys.add(FLASHLIGHT_CALL_PREF);
+                        keys.add(FLASHLIGHT_DND_PREF);
+                        keys.add(FLASHLIGHT_RATE_PREF);
+                    }
 
                     return keys;
                 }
