2a310e9cf14a9dad49e35fe3a0961764d839146c
Martin J. Pollard <martin0566@gmail.com>
2026-02-22 15:58:18 -0500

    Sound: Add adaptive playback

---
+++ src/com/altair/settings/fragments/sound/AdaptivePlayback.java
@@ -0,0 +1,34 @@
+/*
+ * SPDX-FileCopyrightText: 2016-2025 crDroid Android Project
+ * SPDX-FileCopyrightText: 2026 Altair ROM Project
+ * SPDX-License-Identifier: Apache-2.0
+ */
+
+package com.altair.settings.fragments.sound;
+
+import android.content.Context;
+import android.content.ContentResolver;
+import android.os.Bundle;
+import android.os.UserHandle;
+import android.provider.Settings;
+
+import com.android.internal.logging.nano.MetricsProto;
+import com.android.settings.R;
+import com.android.settings.SettingsPreferenceFragment;
+
+public class AdaptivePlayback extends SettingsPreferenceFragment {
+
+    private static final String TAG = AdaptivePlayback.class.getSimpleName();
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+        addPreferencesFromResource(R.xml.adaptive_playback_settings);
+    }
+
+    @Override
+    public int getMetricsCategory() {
+        return MetricsProto.MetricsEvent.ALTAIR_SETTINGS;
+    }
+}
