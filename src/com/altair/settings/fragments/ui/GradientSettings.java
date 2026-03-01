a80d2aa8dcf2aeb737e43b4022f70e11b9631fd9
Martin J. Pollard <martin0566@gmail.com>
2026-02-28 22:50:47 -0500

    UI: Add gradients customization

---
+++ src/com/altair/settings/fragments/ui/GradientSettings.java
@@ -0,0 +1,32 @@
+/*
+ * SPDX-FileCopyrightText: 2026 crDroid Android Project
+ * SPDX-FileCopyrightText: 2026 Altair ROM Project
+ * SPDX-License-Identifier: Apache-2.0
+ */
+
+package com.altair.settings.fragments.ui;
+
+import android.content.ContentResolver;
+import android.content.Context;
+import android.graphics.Color;
+import android.os.Bundle;
+import android.os.UserHandle;
+import android.provider.Settings;
+
+import com.android.internal.logging.nano.MetricsProto;
+import com.android.settings.R;
+import com.android.settings.SettingsPreferenceFragment;
+
+public class GradientSettings extends SettingsPreferenceFragment {
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        addPreferencesFromResource(R.xml.gradient_settings);
+    }
+
+    @Override
+    public int getMetricsCategory() {
+        return MetricsProto.MetricsEvent.ALTAIR_SETTINGS;
+    }
+}
