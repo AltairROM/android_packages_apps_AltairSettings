a1321f658444955dff2491e3287d769c19d67446
Martin J. Pollard <martin0566@gmail.com>
2026-05-03 00:55:27 -0400

    Lockscreen: Pulse: Add subtle haptics on bass spikes

--- src/com/altair/settings/fragments/lockscreen/PulseSettings.java
+++ src/com/altair/settings/fragments/lockscreen/PulseSettings.java
@@ -1,6 +1,6 @@
 /*
- * SPDX-FileCopyrightText: 2016-2025 crDroid Android Project
- * SPDX-FileCopyrightText: 2025 Altair ROM Project
+ * SPDX-FileCopyrightText: crDroid Android Project
+ * SPDX-FileCopyrightText: Altair ROM Project
  * SPDX-License-Identifier: Apache-2.0
  */
 package com.altair.settings.fragments.lockscreen;
@@ -17,17 +17,25 @@ import androidx.preference.PreferenceCategory;
 import androidx.preference.Preference.OnPreferenceChangeListener;
 import androidx.preference.SwitchPreferenceCompat;
 
+import com.altair.settings.utils.DeviceUtils;
 import com.android.internal.logging.nano.MetricsProto;
 import com.android.settings.R;
 import com.android.settings.SettingsPreferenceFragment;
 
 public class PulseSettings extends SettingsPreferenceFragment {
 
+    private static final String KEY_PULSE_BASS_HAPTICS = "pulse_bass_haptics";
+
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
 
         addPreferencesFromResource(R.xml.pulse_settings);
+
+        boolean hapticAvailable = DeviceUtils.hasVibrator(getContext());
+        if (!hapticAvailable) {
+            getPreferenceScreen().removePreference(findPreference(KEY_PULSE_BASS_HAPTICS));
+        }
     }
 
     @Override
