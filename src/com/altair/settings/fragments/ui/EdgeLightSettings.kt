99187353a5e26728353e2bc39b1ebc5b939dc1ed
Martin J. Pollard <martin0566@gmail.com>
2026-05-04 00:16:04 -0400

    UI: Add edge light preview

---
+++ src/com/altair/settings/fragments/ui/EdgeLightSettings.kt
@@ -0,0 +1,27 @@
+/*
+ *  SPDX-FileCopyrightText: crDroid Android Project
+ *  SPDX-FileCopyrightText: Altair ROM Project
+ *  SPDX-License-Identifier: Apache-2.0
+ */
+
+package com.altair.settings.fragments.ui
+
+import android.content.Context
+import android.graphics.Color
+import android.os.Bundle
+import android.os.UserHandle
+import android.provider.Settings
+
+import com.android.internal.logging.nano.MetricsProto
+import com.android.settings.R
+import com.android.settings.SettingsPreferenceFragment
+
+class EdgeLightSettings : SettingsPreferenceFragment() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        addPreferencesFromResource(R.xml.edge_light_settings)
+    }
+
+    override fun getMetricsCategory(): Int = MetricsProto.MetricsEvent.ALTAIR_SETTINGS
+}
